58c8fa6448d9907e2784bbffb0d895f1e6218b98
ipavlakos <pavlakosIlias@outlook.com>
2024-08-15 12:49:42 +0300

    Cross component communication with outputs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {HeaderComponent} from "./header/header.component";
 import {UserInputComponent} from "./user-input/user-input.component";
 
@@ -11,4 +11,43 @@ import {UserInputComponent} from "./user-input/user-input.component";
     UserInputComponent
   ]
 })
-export class AppComponent {}
+export class AppComponent {
+
+
+  onCalculateInvestmentResults(data: {
+    initialInvestment: number,
+    annualInvestment: number,
+    expectedReturn: number,
+    duration: number
+  }) {
+
+    const {
+      initialInvestment,
+      annualInvestment,
+      expectedReturn,
+      duration
+    } = data;
+
+    const annualData = [];
+    let investmentValue = initialInvestment;
+
+    for (let i = 0; i < duration; i++) {
+      const year = i + 1;
+      const interestEarnedInYear = investmentValue * (expectedReturn / 100);
+      investmentValue += interestEarnedInYear + annualInvestment;
+      const totalInterest =
+        investmentValue - annualInvestment * year - initialInvestment;
+      annualData.push({
+        year: year,
+        interest: interestEarnedInYear,
+        valueEndOfYear: investmentValue,
+        annualInvestment: annualInvestment,
+        totalInterest: totalInterest,
+        totalAmountInvested: initialInvestment + annualInvestment * year,
+      });
+    }
+
+    console.log(annualData);
+    return annualData;
+  }
+}
